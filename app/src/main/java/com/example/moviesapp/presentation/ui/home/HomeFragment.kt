a953b5d3b11f6308dff5fcda0a1737f7283edbf7
ioanekareli <johnqareli01@gmail.com>
2022-08-04 20:19:07 +0400

    feature/configure project

---
+++ app/src/main/java/com/example/moviesapp/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.moviesapp.presentation.ui.home
+
+import com.example.moviesapp.databinding.FragmentHomeBinding
+import com.example.moviesapp.presentation.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun init() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/moviesapp/util/Resource.kt
@@ -0,0 +1,15 @@
+package com.example.moviesapp.util
+
+sealed class Resource<out T>{
+
+    class Success<T>(val data:T):Resource<T>()
+
+    class Error<T>(
+        val message:String? = null,
+        val data:T? = null
+    ):Resource<T>()
+
+    object Loading:Resource<Nothing>()
+
+    object Idle : Resource<Nothing>()
+}
